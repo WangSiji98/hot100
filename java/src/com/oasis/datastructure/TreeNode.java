d7a12f564b202346dc9b5db67c24b07c80fdd71d
Siji <807403713@qq.com>
2021-09-07 00:56:11 -0700

    9.6

--- java/src/com/oasis/datastructure/TreeNode.java
+++ java/src/com/oasis/datastructure/TreeNode.java
@@ -5,7 +5,11 @@ public class TreeNode {
      public TreeNode left;
      public TreeNode right;
      public TreeNode() {}
-     public TreeNode(int val) { this.val = val; }
+     public TreeNode(int val) {
+         this.val = val;
+         this.left = null;
+         this.right = null;
+     }
      public TreeNode(int val, TreeNode left, TreeNode right) {
          this.val = val;
          this.left = left;
---
+++ java/src/com/oasis/test/Test2.java
@@ -0,0 +1,11 @@
+package com.oasis.test;
+
+import java.util.Arrays;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int[] i = new int[]{1, 2}, j = new int[]{1, 2};
+        System.out.println(Arrays.equals(i, j));
+    }
+
+}
